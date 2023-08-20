b983a1f77562a43aa058e626f721f68cdb91be54
cesar <haliercesr@gmail.com>
2023-08-20 11:08:13 -0300

    deploy

--- src/controllers/postUser.js
+++ src/controllers/postUser.js
@@ -2,18 +2,18 @@ const {User} =require ('../DB_connection')
 
 const postUser= async (req,res)=>{
     try{
-   const{email,password}=req.body
+   const{email,password,name}=req.body
    
 
-   if (!email || !password) return res.status(400).json({error:'Faltan datos'})
+   if (!email || !password || !name) return res.status(400).json({error:'Faltan datos'})
 
    const [createdUser, created]= await User.findOrCreate({   //model query: busca segun las condiciones en where y si no las encuentra crea una entrada segun las condiciones. Luego devuelve la instancia creada o encontrada.
     where:{email},                                           // default se utiliza en caso de que no se encuentre nada.
     defaults:{                                                //findOrCreated te devuelve un array con dos respuestas, el usuario y un valor booleano que nos dice si se creo o no.
         password,
+        name,
     },
    })
-   console.log(created)
    if (created){
     return res.status(201).json({created})
    } else{
