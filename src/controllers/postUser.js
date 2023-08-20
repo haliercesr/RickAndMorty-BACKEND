fcf13bc2f151989922cb7cbd87c079d12754034d
cesar <haliercesr@gmail.com>
2023-08-20 18:58:52 -0300

    deploy

--- src/controllers/postUser.js
+++ src/controllers/postUser.js
@@ -7,7 +7,8 @@ const postUser= async (req,res)=>{
 
    if (!email || !password || !name) return res.status(400).json({error:'Faltan datos'})
 
-   const [createdUser, created]= await User.findOrCreate({   //model query: busca segun las condiciones en where y si no las encuentra crea una entrada segun las condiciones. Luego devuelve la instancia creada o encontrada.
+   const [createdUser, created]= await User.findOrCreate({   
+                                                              //model query: busca segun las condiciones en where y si no las encuentra crea una entrada segun las condiciones. Luego devuelve la instancia creada o encontrada.
     where:{email},                                           // default se utiliza en caso de que no se encuentre nada.
     defaults:{                                                //findOrCreated te devuelve un array con dos respuestas, el usuario y un valor booleano que nos dice si se creo o no.
         password,
